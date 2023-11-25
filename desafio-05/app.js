8df2bd39c0797e9260e8244af4462b6b26507ca3
Lucas Costa <lucas.costa0117@gmail.com>
2023-11-24 22:35:48 -0300

    feat: implementa resolução do desafio-05

---
+++ desafio-05/app.js
@@ -0,0 +1,23 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		livro: {
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		},
+		marcas: ['Bajaj', 'Honda', 'BMW', 'Triumph'],
+		alt: false,
+	},
+	methods: {
+		isArray(array) {
+			return Array.isArray(array);
+		}
+	}
+});
