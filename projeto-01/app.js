e5fe1e5b04ff7d8328e51d9d2cd8e2f76187909e
Lucas Costa <lucas.costa0117@gmail.com>
2024-01-02 09:56:49 -0300

    feat: implementa função de 'Ataque' com probabilidade maior do 'Monstro' ganhar

--- projeto-01/app.js
+++ projeto-01/app.js
@@ -2,6 +2,9 @@ new Vue({
     el: '.app',
     data: {
         inGame: false,
+        hasLog: false,
+        playerHealth: 100,
+        monsterHealth: 100,
     },
     methods: {
         startGame() {
@@ -9,6 +12,22 @@ new Vue({
         },
         finishGame() {
             this.inGame = !this.inGame;
+            this.hasLog = false;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        normalAttack() {
+            if (this.playerHealth > 0 && this.monsterHealth > 0) {
+                this.playerHealth -= Math.floor((Math.random() *10) + 8);
+                if (this.playerHealth < 0) {
+                    this.playerHealth = 0;
+                };
+                
+                this.monsterHealth -= Math.floor((Math.random() *10) + 5);
+                if (this.monsterHealth < 0) {
+                    this.monsterHealth = 0;
+                };
+            }
         }
     }
 });
